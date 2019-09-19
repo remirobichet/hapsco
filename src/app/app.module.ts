f7235c591c7ef0e9dd7854b6deb22eabf5957e3c
Rémi Robichet <remi.robichet@gmail.com>
2019-09-19 19:52:41 +0200

    Revert ":lipstick: Add angular material style"
    This reverts commit 417ad60a767bc01674110bec0e9ae785d4ced0ae.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { ChartsModule } from 'ng2-charts';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,9 +25,7 @@ import { EditComponent } from './edit/edit.component';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     ChartsModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MatTableModule
+    FormsModule
   ],
   providers: [
     AngularFirestore,
