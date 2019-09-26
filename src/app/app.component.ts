09ea7b79731c7823640c8c50cc310c04a6084105
Rémi Robichet <remi.robichet@cgi.com>
2019-09-26 11:32:34 +0200

    :sparkles: Title change depending on active page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Title } from "@angular/platform-browser";
 import { filter } from 'rxjs/operators';
 
 @Component({
@@ -11,14 +12,15 @@ export class AppComponent implements OnInit{
   title: string = 'Hapsco';
   titleDescritpion: string;
 
-  constructor(private router: Router, private route:ActivatedRoute) { }
+  constructor(private router: Router, private route:ActivatedRoute, private titleService: Title) { }
 
   ngOnInit() {
     this.router.events.pipe(
-        filter(event => event instanceof NavigationEnd)
+      filter(event => event instanceof NavigationEnd)
     )
     .subscribe(() => {
-        this.titleDescritpion = this.route.root.firstChild.snapshot.data['title'];
+      this.titleDescritpion = this.route.root.firstChild.snapshot.data['title'];
+      this.titleService.setTitle(this.title + ' | ' + this.titleDescritpion);
     });
 }
 }
